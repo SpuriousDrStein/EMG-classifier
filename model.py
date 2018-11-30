0e3199a35755251e81f1769d962b95d9676c6031
Spurious Dr. Stein <35902579+SpuriousDrStein@users.noreply.github.com>
2018-11-30 01:48:12 +0000

    Update model.py

--- model.py
+++ model.py
@@ -26,40 +26,12 @@ class processer:
 
             nn[0] = tf.layers.conv1d(x, c[1], k_size, padding='SAME', kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.constant(0), activation=tf.nn.relu)
             nn[0] = tf.layers.max_pooling1d(nn[0], pool_size, 2)
-            print(nn[0].shape)
             for d in range(1, self.depth-1):
                 nn[d] = tf.layers.conv1d(nn[d-1], c[d+1], k_size, padding='SAME', kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.constant(0), activation=tf.nn.relu)
                 nn[d] = tf.layers.max_pooling1d(nn[d], pool_size, 2)
-                print(nn[d].shape)
             nn[-1] = tf.layers.conv1d(nn[-2], c[-1], k_size, padding='SAME', kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.constant(0), activation=tf.nn.relu)
             nn[-1] = tf.layers.max_pooling1d(nn[-1], pool_size, 2)
-            print(nn[-1].shape)
         return nn[-1]
-
-    def decoder(self, z, k_size=5, reuse=False):
-        with tf.variable_scope('DECODER'):
-            if reuse:
-                tf.get_variable_scope().reuse_variables()
-            nn = vv_w = vv_b = [None for _ in range(self.depth)]
-            c = [int(self.z_shape[1]/pow(2,d)) for d in range(0, self.depth+1)] # z_channels -> self.z_shape[0]
-            width = [*reversed(c)] # reversed([self.input_size/pow(2,d) for d in range(0, self.depth+1)])
-
-            vv_w[0] = tf.get_variable('filter_w_1', shape=(k_size, c[1], c[0]))
-            vv_b[0] = tf.get_variable('filter_b_1', shape=(k_size))
-            nn[0] = tf.nn.relu(tf.nn.bias_add(conv1d_transpose(z, filter=vv_w[0], output_shape=(self.batch_size, width[0], c[1]), stride=2), vv_b[0]))
-            print(nn[0].shape)
-            for d in range(1, self.depth-1):
-                vv_w[d] = tf.get_variable('filter_w_'+str(d+1), shape=(k_size, c[d+1], c[d]))
-                vv_b[d] = tf.get_variable('filter_b_'+str(d+1), shape=(k_size))
-                nn[d] = tf.nn.relu(tf.nn.bias_add(conv1d_transpose(nn[d-1], filter=vv_w[d], output_shape=(self.batch_size, width[d], c[d+1]), stride=2), vv_b[d]))
-                print(nn[d].shape)
-
-            vv_w[-1] = tf.get_variable('filter_w_'+str(self.depth), shape=(k_size, c[-1], c[-2]))
-            vv_b[-1] = tf.get_variable('filter_b_'+str(self.depth), shape=(k_size))
-            nn[-1] = tf.nn.sigmoid(tf.nn.bias_add(conv1d_transpose(nn[-1], filter=vv[-1], output_shape=(self.batch_size, width[-1], c[-1]), stride=2), vv_b[-1]))
-            print(out.shape)
-
-        return out
 
     def fully_connected(self, x, h_layer_sizes=(200,200), reuse=False):
         with tf.variable_scope('CLASSIFIER'):
@@ -70,32 +42,23 @@ class processer:
 
             h_layers[0] = tf.layers.dense(x, h_layer_sizes[0], activation=tf.nn.relu)
             for i in range(len(h_layer_sizes)-1):
-                tf.layers.dense(h_layer_sizes[i], h_layer_sizes[i+1], activation=tf.nn.relu)
+                h_layers[i+1] = tf.layers.dense(h_layers[i], h_layer_sizes[i+1], activation=tf.nn.relu)
             out = tf.nn.softmax(h_layers[-1])
 
         return out
 
-    # def rnn(self, x, reuse=False):
-    #     with tf.variable_scope('RNN'):
-    #         if reuse:
-    #             tf.get_variable_scope().reuse_variables()
-            
-    #         H = tf.get_variable('H', shape=(self.z_shape[0], self.h_size), initializer=tf.initializers.random_normal)
 
-    #         xh_w = tf.get_variable('xh_w', shape=(self.z_shape[1], self.h_size), initializer=tf.initializers.random_normal)
-    #         xh_b = tf.get_variable('xh_b', shape=(self.z_shape[1]), initializer=tf.initializers.constant(0))
-    #         hh_w = tf.get_variable('hh_w', shape=(self.h_size, self.h_size), initializer=tf.initializers.random_normal)
-    #         hh_b = tf.get_variable('hh_b', shape=(self.h_size), initializer=tf.initializers.constant(0))
-    #         hy_w = tf.get_variable('hy_w', shape=(self.h_size, self.z_shape[1]), initializer=tf.initializers.random_normal)
-    #         hy_b = tf.get_variable('hy_b', shape=(self.h_size), initializer=tf.initializers.constant(0))
+def get_label():
+    while True:
+        for l in range(1,num_actions+1):
+            print(actionspace[l-1],' = ', l)
+        print('select label: ')
+        inp = input()
+        if int(inp)<=num_actions and int(inp)>=1:
+            return actionspace[int(inp)-1]
+        else:
+            continue
 
-    #         # propergate input
-    #         xh_h = tf.nn.sigmoid((x*xh_w + xh_b) + H)
-    #         h_y  = tf.nn.sigmoid(xh_h*hy_w + hy_b)
-
-    #         # propergate hidden
-    #         H    = tf.nn.tanh(H*hh_w + hh_b)
-    #     return h_y
 
 
 # HPs
@@ -104,31 +67,24 @@ num_recordings = 1
 batch_size = 32
 z_chan = 64
 h_size = 64
-
 num_actions = 3
+enc_action_space = np.identity(num_actions, dtype=float)
+port = '\\.\COM3'
 
-actionspace = ['a','b','n'] # has to be length num_actions
-enc_action_space = np.identity(num_actions, dtype=tf.float32)
-
-# SERIAL
-serial_connection = ser.Serial('COM7')
-print('serial port: ', serial_connection.name, ' used')
 
 # MODEL
 #   PLACEHOLDERs
 wm = processer(z_chan, h_size, depth=5, input_size=batch_size)
-selected_actions = []
-action_accuracy = {}
 
-x1 = tf.placeholder(name='X1', shape=(None, batch_size, 1), dtype=tf.float32)
-x2 = tf.placeholder(name='X2', shape=(None, batch_size, 1), dtype=tf.float32)
-x3 = tf.placeholder(name='X3', shape=(None, batch_size, 1), dtype=tf.float32)
-x4 = tf.placeholder(name='X4', shape=(None, batch_size, 1), dtype=tf.float32)
-x5 = tf.placeholder(name='X5', shape=(None, batch_size, 1), dtype=tf.float32)
-x6 = tf.placeholder(name='X6', shape=(None, batch_size, 1), dtype=tf.float32)
+x1 = tf.placeholder(name='X1', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
+x2 = tf.placeholder(name='X2', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
+x3 = tf.placeholder(name='X3', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
+x4 = tf.placeholder(name='X4', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
+x5 = tf.placeholder(name='X5', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
+x6 = tf.placeholder(name='X6', shape=(num_recordings, batch_size, 1), dtype=tf.float32)
 
-z = tf.placeholder(name='z', shape=(None, NUM_CHANNELS, *wm.z_shape), dtype=tf.float32)
-label = tf.placeholder(name='label', shape=(num_actions))
+z = tf.placeholder(name='z', shape=(num_recordings, NUM_CHANNELS, *wm.z_shape), dtype=tf.float32)
+label = tf.placeholder(name='label', shape=(num_actions), dtype=tf.float32)
 
 #   ADDITIONAL VARIABLES
 gamma = 5
@@ -148,41 +104,41 @@ nx4 = (x4 - mean4)/tf.sqrt(tf.square(var4)+0.00001) * gamma + beta
 nx5 = (x5 - mean5)/tf.sqrt(tf.square(var5)+0.00001) * gamma + beta
 nx6 = (x6 - mean6)/tf.sqrt(tf.square(var6)+0.00001) * gamma + beta
 
-xx1, xx2, xx3, xx4, xx5, xx6 = [wm.encoder(nx1), wm.encoder(nx2, reuse=True), wm.encoder(nx3, reuse=True), wm.encoder(nx4, reuse=True), wm.encoder(nx5, reuse=True), wm.encoder(nx6, reuse=True)] # possible attention
-z = tf.concat(1, (xx1,xx2,xx3,xx4,xx5,xx6))
-prediction = wm.fully_connected(z, h_layer_sizes=(100, 200, 100, 50, 25 ,num_actions))
-prediction_error = tf.losses.softmax_cross_entropy(label, prediction)
-
-optimizer = tf.train.AdamOptimizer(learning_rate=0.003).minimize(prediction_error, var_list=tf.trainable_variables)
-
+xx1, xx2, xx3, xx4, xx5, xx6 = wm.encoder(nx1), wm.encoder(nx2, reuse=True), wm.encoder(nx3, reuse=True), wm.encoder(nx4, reuse=True), wm.encoder(nx5, reuse=True), wm.encoder(nx6, reuse=True)
+z = tf.concat(axis=0, values=[xx1,xx2,xx3,xx4,xx5,xx6])
+prediction = wm.fully_connected(z, h_layer_sizes=(75, 100, 50, 25, 12 ,num_actions))
+prediction_error = sum([(tf.losses.softmax_cross_entropy(label, tf.squeeze(prediction[u]))) for u in range(NUM_CHANNELS)])
+optimizer = tf.train.AdamOptimizer(learning_rate=0.003).minimize(prediction_error, var_list=tf.trainable_variables())
 
 
-def get_label():
-    while True:
-        for l in range(1,num_actions+1):
-            print(actionspace[l-1],' = ', l)
-        print('select lable: ')
-        inp = input()
-        if int(inp)<=num_actions and int(inp)>=1:
-            return actionspace[l-inp]
-        else:
-            continue
-
-
+# TRAINING
+actionspace = ['a','b','n']
 training = True
+iterations = 100
+past_actions = np.empty([0, NUM_CHANNELS])
+
+serial_connection = ser.Serial(port=port, baudrate=9600)
+print('serial port: ', serial_connection.name, ' used')
+
+#   LOOP
 with tf.Session() as sess:
+    if len(actionspace) != num_actions:
+        raise(AttributeError)
+
     sess.run(tf.global_variables_initializer())
 
     if training:
-        # GET LABEL FOR t
-        action = get_label()       
-        sel_action.append(action)
+        #for i in range(iterations):
 
-        # GET AND RUN
+            # action_label = get_label()
+        print(serial_connection.read(6*6))
 
-        # schedule:
-        a1, a2, a3, a4, a5, a6 = np.reshape(serial_connection.read(NUM_CHANNELS * batch_size), [NUM_CHANNELS, batch_size])
+            # a1, a2, a3, a4, a5, a6 = np.reshape(serial_connection.read(NUM_CHANNELS * batch_size), [NUM_CHANNELS, batch_size])
+            # print(a1,a2,a3,a4,a5,a6)
+            # _, pred, pred_err = sess.run([optimizer, prediction, prediction_error], feed_dict={'X1:0': a1, 'X2:0': a2, 'X3:0': a3, 'X4:0': a4, 'X5:0': a5, 'X6:0': a6, 'label:0':action_label})
+            
+            # np.append(past_actions, [a1,a2,a3,a4,a5,a6], 0)
+            # # past_actions -> encode in temporal context (RNN) -> "env representation" -> policy network -> action_label_2
 
-        sess.run(enc, feed_dict={'X1:0': a1, 'X2:0': a2, 'X3:0': a3, 'X4:0': a4, 'X5:0': a5, 'X6:0': a6})
 
 
